797e153e84825eacf3b3fb25b8ad9b8e1966b312
천진아 <jncheon@carmerce.co.kr>
2025-12-17 22:49:24 +0900

    feat: 클라이언트 사이드 hydration 구현
    - __INITIAL_DATA__에서 서버 데이터 복원
    - 서버 데이터가 있으면 불필요한 재요청 방지
    - productStore에 초기 상태 주입

--- packages/vanilla/src/main.js
+++ packages/vanilla/src/main.js
@@ -4,6 +4,7 @@ import { registerAllEvents } from "./events";
 import { loadCartFromStorage } from "./services";
 import { router } from "./router";
 import { BASE_URL } from "./constants.js";
+import { productStore, PRODUCT_ACTIONS } from "./stores";
 
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker }) =>
@@ -16,6 +17,23 @@ const enableMocking = () =>
   );
 
 function main() {
+  // 서버 데이터 복원
+  if (window.__INITIAL_DATA__) {
+    const data = window.__INITIAL_DATA__;
+    if (data.products)
+      productStore.dispatch({
+        type: PRODUCT_ACTIONS.SETUP,
+        payload: {
+          products: data.products,
+          categories: data.categories,
+          totalCount: data.totalCount,
+          loading: false,
+          status: "done",
+        },
+      });
+    delete window.__INITIAL_DATA__;
+  }
+
   registerAllEvents();
   registerGlobalEvents();
   loadCartFromStorage();
